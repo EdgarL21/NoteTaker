b032b83e9ce9587010deeca0e683202e58a05005
Edgar Lule <edgar21lc@gmail.com>
2021-06-15 06:38:25 -0700

    updated readme

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const notes = require("./db/db.json");
+// const notes = require("./db/db.json");
 const uuid = require("uuid");
 
 const app = express();
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 // changes from hexadeciaml to json back and forth
-//Middleware // lets us have access to our html css and js
+// Middleware // lets us have access to our html css and js
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // this made it so public folder // this removed the first error
@@ -37,71 +37,30 @@ app.get("/api/notes", (request, response) => {
   response.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-// app.get('/api/notes', (req, res) => {
-//   res.json(noteDB);
-// });
 
 app.post("/api/notes", (request, response) => {
   console.log("You are in the post request");
   // console.log("request.body");
   // console.log(request.body)
   const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-
-  // console.log('-----./db-----');
-  // console.log(./db/db.json)
-  // console.log('-----./db-----');
-
-  console.log("-----Parse-----");
   console.log(fs.readFileSync("./db/db.json")); // logs the text being converted into hexadeciaml
-  console.log("-----Parse-----");
-
-  // console.log('-----JSONParse-----');
-  // console.log(JSON.parse(fs.readFileSync("./db/db.json"))) // logs the text being converted into hexadeciaml
-  // console.log('-----JSONParse-----');
-
-  console.log("---------------notes:---------------");
   console.log(notes); // logs all the objects in the current api
-  console.log("---------------notes:---------------");
-
   const newNotes = request.body;
-
-  console.log("---------------newNotes:---------------");
   console.log(newNotes); // logs the current object that was typed in the browser
-  console.log("---------------newNotes:---------------");
-
   newNotes.id = uuid.v4(); // gives an id to each object
-  notes.push(newNotes); // pushes the mpst recent added note into the notes array(database)
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes)); // strinfifies the new array of obejcts
+  notes.push(newNotes); // pushes the most recent added note into the notes array(database)
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes)); // stringifies the new array of obejcts
   response.json(notes); // sends back a json object
 });
 
 app.delete("/api/notes/:id", (request, response) => {
   console.log("You are in the delete request");
-
   const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-
-  // console.log('-----./db-----');
-  // console.log(./db/db.json)
-  // console.log('-----./db-----');
-
-  console.log("-----Parse-----");
-  console.log(fs.readFileSync("./db/db.json"));
-  console.log("-----Parse-----");
-
-  console.log("-----DELETE notes-----");
   console.log(notes); // logs all the current objects before one being deleted
-  console.log("-----DELETE notes-----");
-
   const delNote = notes.filter((rmvNote) => rmvNote.id !== request.params.id);
-  console.log("-----DELETE delNote-----");
   console.log(delNote); // logs all the current obejcts left after one of them was deleted
-  console.log("-----DELETE delNote-----");
-
   fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
-  console.log("JSON.stringify(delNote)");
   console.log(JSON.stringify(delNote)); // logs the current objects after one of them was deleted but all as a string
-  console.log("JSON.stringify(delNote)");
-
   response.json(delNote);
   console.log(response.json(delNote)); // logs the current objects after one of them was deleted but as json
 });
